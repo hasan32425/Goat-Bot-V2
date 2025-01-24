8afd13acb5a87d3ad8671d2ecc17dee8d9df5757
Itz HaSan <146573168+hasan32425@users.noreply.github.com>
2025-01-24 10:07:00 +0600

    Update gen.js

--- scripts/cmds/gen.js
+++ scripts/cmds/gen.js
@@ -1,82 +1,59 @@
-const models = [
-  'DreamShaper',
-  'MBBXL_Ultimate',
-  'Mysterious',
-  'Copax_TimeLessXL',
-  'Pixel_Art_XL',
-  'ProtoVision_XL',
-  'SDXL_Niji',
-  'CounterfeitXL',
-  'DucHaiten_AIart_SDXL'
-];
+const axios = require('axios');
 
 module.exports = {
   config: {
     name: "gen",
-    version: "1.0",
-    author: "RedWan×MAHI×Sanam",
-    countDown: 5,
+    aliases: ["generate","imagine"],
+    version: "1.1",
+    author: "JARiF",
+    countDown: 10,
     role: 0,
+    shortDescription: {
+      en: 'Text to Image'
+    },
     longDescription: {
-      vi: "",
-      en: "Get images from text.",
+      en: "Text to image"
     },
-    category: "Image~Create",
+    category: "image",
     guide: {
-      vi: "",
-      en: "Type {pn} with your prompts | (model name)\nHere are the Supported models:\n" + models.map((item, index) => `${index + 1}. ${item}`).join('\n'),
-    },
-  },
-
-  onStart: async function ({ api, args, message, event }) {
-    try {
-      const text = args.join(" ");
-      if (!text) {
-        return message.reply("Please provide a prompt.");
-      }
-
-      let prompt, model;
-      if (text.includes("|")) {
-        const [promptText, modelText] = text.split("|").map((str) => str.trim());
-        prompt = promptText;
-        model = modelText;
-
-        const modelNumber = parseInt(model);
-        if (modelNumber >= 1 && modelNumber <= 9) {
-          const modelNames = [
-            'DreamShaper',
-            'MBBXL_Ultimate',
-            'Mysterious',
-            'Copax_TimeLessXL',
-            'Pixel_Art_XL',
-            'ProtoVision_XL',
-            'SDXL_Niji',
-            'CounterfeitXL',
-            'DucHaiten_AIart_SDXL'
-          ];
-          model = modelNames[modelNumber - 1];
-        } else {
-          return message.reply("Invalid model number. Supported models are:\n" + models.map((item, index) => `${index + 1}. ${item}`).join('\n'));
-        }
-      } else {
-        prompt = text;
-        model = "DreamShaper";
-      }
-
-      let id;
-      api.setMessageReaction("⏳", event.messageID, () => {}, true);
-      const waitingMessage = await message.reply("✅ | Creating your Imagination...");
-
-      const API = `https://www.api.vyturex.com/curios?prompt=${encodeURIComponent(prompt)}&modelType=${model}`;
-      const imageStream = await global.utils.getStreamFromURL(API);
-
-      await message.reply({
-        attachment: imageStream,
-      });
-      api.setMessageReaction("✅", event.messageID, () => {}, true);
-      await api.unsendMessage(waitingMessage.messageID);
-    } catch (error) {
-      message.reply("Your prompt is blocked. Try again later with another prompt. [ Tor Mayre Chudi REDWAN/ MAHI  Othoba Sanam Er Permission Nicos? ]");
+      en: 'Type -gen with your prompts | (number which model do you want)\nHere are the Supported models:\n1. Analog-diffusion-1.0\n2. Anythingv3_0\n3. Anything-v4.5\n4. Anything-V5\n5. AOM3A3_Orangemix\n6. Deliberate_v2\n7. Dreamlike-diffusion-1.0\n8. Dreamlike-diffusion-2.0\n9. Dreamshaper_5BakedVae\n10. Dreamshaper_6BakedVae\n11. Dreamshaper_7\n12. Elldreths-vivid-mix\n13. Lyriel_v15\n14. Lyriel_v16\n15. Mechamix_v10\n16. Meinamix_meinaV9\n17. Openjourney_V4\n18. Portrait+1.0\n19. PortraitPlus_V1.0\n20. Realistic_Vision_V1.4\n21. Realistic_Vision_V2.0\n22. Realistic_Vision_V4.0\n23. RevAnimated_v122\n24. Riffusion-Model-V1\n25. Sdv1_4\n26. V1-5\n27. ShoninsBeautiful_v10\n28. Theallys-mix-ii\n29. Timeless-1.0\n30. EimisAnimeDiffusion_V1.0\n31. Meinamix_v11'
     }
   },
+
+  onStart: async function ({ message, args }) {
+    const text = args.join(" ");
+    if (!text) {
+      return message.reply("Please provide a prompt.");
+    }
+
+    let prompt, model;
+    if (text.includes("|")) {
+      const [promptText, modelText] = text.split("|").map((str) => str.trim());
+      prompt = promptText;
+      model = modelText;
+    } else {
+      prompt = text;
+      model = "22";  
+    }
+
+    let id; 
+
+    message.reply("✅| Creating your Imagination...").then((info) => {
+      id = info.messageID;  
+    });
+
+    try {
+      const API = `https://gen.blackxlegend1.repl.co/imagine?model=${model}&prompt=${encodeURIComponent(prompt)}&apikey=emma_heesters_quiin`;
+      const imageStream = await global.utils.getStreamFromURL(API);
+
+      return message.reply({
+        attachment: imageStream
+      });
+    } catch (error) {
+      console.log(error);
+      message.reply("Failed to generate your imagination.").then(() => {
+        message.delete(id);
+      });
+    }
+  }
 };
