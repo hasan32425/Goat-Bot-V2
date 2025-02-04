24fc5b6d6f7cbf7012079d979ade8a8e13a87a9b
Itz HaSan <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-02-04 20:22:35 +0600

    Update adminonly.js

--- scripts/cmds/adminonly.js
+++ scripts/cmds/adminonly.js
@@ -4,22 +4,22 @@ const { client } = global;
 
 module.exports = {
 	config: {
-		name: "adminonly",
-		aliases: ["adonly", "onlyad", "onlyadmin"],
-		version: "1.5",
-		author: "NTKhang",
+		name: "owneronly",
+		aliases: ["ownonly", "onlyown", "onlyowner"],
+		version: "1.0",
+		author: "hasan",
 		countDown: 5,
 		role: 2,
 		description: {
 			vi: "bật/tắt chế độ chỉ admin mới có thể sử dụng bot",
-			en: "turn on/off only admin can use bot"
+			en: "turn on/off only owner can use bot"
 		},
 		category: "owner",
 		guide: {
 			vi: "   {pn} [on | off]: bật/tắt chế độ chỉ admin mới có thể sử dụng bot"
 				+ "\n   {pn} noti [on | off]: bật/tắt thông báo khi người dùng không phải là admin sử dụng bot",
-			en: "   {pn} [on | off]: turn on/off the mode only admin can use bot"
-				+ "\n   {pn} noti [on | off]: turn on/off the notification when user is not admin use bot"
+			en: "   {pn} [on | off]: turn on/off the mode only owner can use bot"
+				+ "\n   {pn} noti [on | off]: turn on/off the notification when user is not owner use bot"
 		}
 	},
 
@@ -31,10 +31,10 @@ module.exports = {
 			turnedOffNoti: "Đã tắt thông báo khi người dùng không phải là admin sử dụng bot"
 		},
 		en: {
-			turnedOn: "Turned on the mode only admin can use bot",
-			turnedOff: "Turned off the mode only admin can use bot",
-			turnedOnNoti: "Turned on the notification when user is not admin use bot",
-			turnedOffNoti: "Turned off the notification when user is not admin use bot"
+			turnedOn: "Turned on the mode only owner can use bot",
+			turnedOff: "Turned off the mode only owner can use bot",
+			turnedOnNoti: "Turned on the notification when user is not owner use bot",
+			turnedOffNoti: "Turned off the notification when user is not owner use bot"
 		}
 	},
 
@@ -56,11 +56,11 @@ module.exports = {
 			return message.SyntaxError();
 
 		if (isSetNoti) {
-			config.hideNotiMessage.adminOnly = !value;
+			config.hideNotiMessage.ownerOnly = !value;
 			message.reply(getLang(value ? "turnedOnNoti" : "turnedOffNoti"));
 		}
 		else {
-			config.adminOnly.enable = value;
+			config.ownerOnly.enable = value;
 			message.reply(getLang(value ? "turnedOn" : "turnedOff"));
 		}
 
